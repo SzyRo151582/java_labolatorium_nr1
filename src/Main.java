aa8ecc779785af284b1f59c03f626f2b51dca9d0
SzyRo151582 <szomyn3@gmail.com>
2021-10-21 12:33:13 +0200

    Pierwsze zadanie w z 1 labolatorium.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
 //        Użyć każdej z opcji
 
 System.out.println("Dzień dobry!");
+System.out.printf("Oto mój pierwszy program w Javie!\n");
+System.out.printf("Mam nadzieję że nasz %s czas będzie bardzo produktywny." ,"wspólny");
+System.out.print("Pozdrawiam");
+System.out.print(" Pana");
+System.out.print(" bardzo" + " serdecznie.");
+
     }
 }
