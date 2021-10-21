e116db36eeefe7dafc53154d944d490ffd6f3e84
SzyRo151582 <szomyn3@gmail.com>
2021-10-21 15:05:23 +0200

    Piąte zadanie w z 1 labolatorium.

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,45 @@ public class Main {
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Wprowadź swój wiek: ");
+        int mojWiek = scan.nextInt();
+
+        if (mojWiek % 3 == 0)
+        {
+            System.out.println("Podzielny przez 3");
+        }
+        else
+        {
+            System.out.println("Niepodzielny przez 3");
+        }
+
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+
+        System.out.print("Wprowadź swój numer indeksu: ");
+        int mojNumerIndeksu = scan.nextInt();
+
+        int wynik = (mojNumerIndeksu % 2 == 0) ? 0 : 1;
+        System.out.println(wynik);
 
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
+
+        System.out.print("Wprowadź dowolną liczbę rzeczywistą: ");
+        double mojaLiczba = scan.nextDouble();
+
+        if (mojaLiczba > 0)
+        {
+            System.out.println("Liczba ta jest większa od zera");
+        }
+        else if (mojaLiczba < 0)
+        {
+            System.out.println("Liczba ta jest mniejsza od zera");
+        }
+        else System.out.println("Liczba ta jest równa zero");
 
     }
 }
