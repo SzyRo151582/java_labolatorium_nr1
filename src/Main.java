7623b30c9473423011bfd90dd2b1401f51909bd1
SzyRo151582 <szomyn3@gmail.com>
2021-10-21 13:04:52 +0200

    Drugie zadanie w z 1 labolatorium.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,17 @@ public class Main {
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
 
+        byte zmiennaTypuByte = 13;
+        short zmiennaTypuShort = -2367;
+        int zmiennaTypuInt = 3859746;
+        long zmiennaTypuLong = -582785979589L;
+        float zmiennaTypuFloat = 12.6998f;
+        double zmiennaTypuDouble = -34581.2339;
+        boolean zmiennaTypuBoolean = false;
+        char zmiennaTypuChar = 'u';
+        String zmiennaString = "Witam";
+
+
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
         *    mnożenia, dziealenia i modulo na zmiennych:
@@ -18,6 +29,14 @@ public class Main {
         *  */
 
 
+        int liczbaA = 18 , liczbaB = -8;
+        double liczbaX = 2.8 , liczbaY = -8.12;
+
+        double wynik1 = liczbaA + liczbaX;
+        double wynik2 = liczbaX - liczbaY;
+        int wynik3 = liczbaA * liczbaB;
+        int wynik4 = liczbaB / liczbaA;
+        int wynik5 = liczbaA % liczbaB;
 
     }
 }
