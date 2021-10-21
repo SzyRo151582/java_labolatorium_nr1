3fb5cb0e012f598aa28028b0cd3315486b231687
SzyRo151582 <szomyn3@gmail.com>
2021-10-21 14:22:24 +0200

    Czwarte zadanie w z 1 labolatorium.

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        boolean zlozonaOperacja1 = (a && b) || c;
+        System.out.println("Wynik pierwszej operacji to: " +zlozonaOperacja1);
+
+        boolean zlozonaOperacja2 = (!(b || c)) && (c && a);
+        System.out.println("Wynik drugiej operacji to: " +zlozonaOperacja2);
+
+        boolean zlozonaOperacja3 = (!c || a) || (!(a && b));
+        System.out.println("Wynik trzeciej operacji to: " +zlozonaOperacja3);
+
     }
 }
