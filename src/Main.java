6e37962ac1c39de3348fcef62e8819a41799332d
SzyRo151582 <szomyn3@gmail.com>
2021-10-21 15:20:33 +0200

    Szóste zadanie w z 1 labolatorium.

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,19 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] tab1 = new double[5];
+
+        tab1[0] = 3.14;
+        tab1[1] = -8.18;
+        tab1[2] = 12.48;
+        tab1[3] = -5.155;
+        tab1[4] = 4.99617;
+
+        System.out.println("tab1[0]: " +tab1[0]);
+        System.out.println("tab1[1]: " +tab1[1]);
+        System.out.println("tab1[2]: " +tab1[2]);
+        System.out.println("tab1[3]: " +tab1[3]);
+        System.out.println("tab1[4]: " +tab1[4]);
+
     }
 }
